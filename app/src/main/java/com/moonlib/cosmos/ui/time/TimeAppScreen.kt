afb3ba6bbd553f86e5d847316de2b3259f1edb64
Mooooooon <maoxianmoon@gmail.com>
2026-06-01 17:03:04 +0800

    Fix time note keyboard overlap

--- app/src/main/java/com/moonlib/cosmos/ui/time/TimeAppScreen.kt
+++ app/src/main/java/com/moonlib/cosmos/ui/time/TimeAppScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -60,6 +61,7 @@ fun TimeAppScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
+                .imePadding()
                 .verticalScroll(rememberScrollState())
                 .padding(horizontal = 16.dp, vertical = 8.dp),
             verticalArrangement = Arrangement.spacedBy(20.dp)
